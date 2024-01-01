12f145e3539861587dd5c67b62c21ab81d850cee
Linh Nguyen <linhnguy@c1r3p3.hive.fi>
2024-01-01 18:05:03 +0200

    start .h and main.c

---
+++ push_swap/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: linhnguy <linhnguy@hive.student.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 15:45:25 by linhnguy          #+#    #+#             */
+/*   Updated: 2024/01/01 16:09:12 by linhnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	int				num;
+	struct s_list	*next;
+	struct s_list	*prev;
+}	t_list;
+
+#endif
